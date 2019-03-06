2e32ea0bf57c2d8b5d5c7067019d596b3a433342
philippeHelloWorld.class-richard <philippe_richard@fr.ibm.com>
2019-03-06 19:28:16 +0100

    Commit. replace bright by zowe

--- test/test.js
+++ test/test.js
@@ -58,7 +58,7 @@ describe('Hello World', function () {
             cmd.get(
               'zowe jobs view sfbi ' + data + ' 101 -u $userid --pw $password --ru false',
               function (err, data, stderr) {
-                assert.equal(data.trim(), "HELLO WORLD!");
+                assert.equal(data.trim(), " HELLO WORLD!");
                 done();
               }
             );
